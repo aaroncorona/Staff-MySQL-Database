18fe1f43b1e8b2a0779d8bd5995bc555bfc20066
aaroncorona <amc1140@berkeley.edu>
2022-11-29 21:01:30 -0800

    removing unneeded test logic

--- src/main/java/com/example/aaroncorona_cs56_proj9/view/Main.java
+++ src/main/java/com/example/aaroncorona_cs56_proj9/view/Main.java
@@ -11,14 +11,5 @@ public class Main {
         // Create Staff DB and Staff Table
         StaffService.createStaffDatabase();
         StaffService.createStaffTable();
-//        // Test TODO delete
-//        int randomId = new Random().nextInt(100);
-//        Staff staff = new Staff(50, "Truffle", "Garcia", "C", "home",
-//                "LA", 310, "teddy@gmail.com");
-//        StaffService.insertStaffRecord(staff);
-//        System.out.println(StaffService.getStaffRecordByID(50));
-//        staff.setFirstName("Teddy #" + randomId);
-//        StaffService.updateStaffRecord(staff);
-//        System.out.println(StaffService.getStaffRecordByID(50));
     }
 }
